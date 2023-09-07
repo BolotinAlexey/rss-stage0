de7ebea0254208546ec70f4a5bf6151c1054e004
Oleksii Bolotin <samkiral73@gmail.com>
2023-09-07 11:48:35 +0200

    feat: complete favorites slider without season inputs sticky

--- library/slider.js
+++ library/slider.js
@@ -1,51 +1,51 @@
 const SHOW_DELAY = 6000;
 
-let currentArticle = 0;
+let idInterval,
+  currentArticle = 0;
 const seasonBtns = document.querySelectorAll('.season-input');
 const seasonArticles = document.querySelectorAll('.favorite__articles');
 
-const idTimeout = new Array(4);
-const idInterval = new Array(4);
-
-const onClickSeasonBtn = e => {
-  //   console.log(e.target.dataset.numseason);
+// run showArticle() function every SHOW_DELAY ms
+const handleInterval = () => {
+  idInterval && clearInterval(idInterval);
+  idInterval = setInterval(() => {
+    currentArticle = ++currentArticle % 4;
+    showArticle();
+  }, SHOW_DELAY);
 };
 
-// const showArticle = () => {
-//   // const currentArticle = document.querySelector(`[data-seasonart='${i}']`);
-//   document.querySelector('.show-article').classList.remove('show-article');
+// handler of season buttons press
+const onClickSeasonBtn = e => {
+  currentArticle = e.target.dataset.numseason;
+  handleInterval();
+  showArticle();
+};
 
-//   seasonArticles[++currentSlide % 4].classList.add('show-article');
-// };
-
-// const setIntervalCustom = i => {
-//     showArticle(i);
-//   idInterval[i] = setInterval(showArticle, SHOW_DELAY * 4, i);
-// };
-
+// fadout previous articles and fadein next atticles and change checked button
 const showArticle = () => {
+  // fadein-fadout articles
   const prev = document.querySelector('.show-article');
   prev.classList.remove('show-article');
   prev.classList.add('hide-article');
-  currentArticle = ++currentArticle % 4;
   const current = seasonArticles[currentArticle];
-  if (current.classList.contains('hide-article'))
+  current.classList.contains('hide-article') &&
     current.classList.remove('hide-article');
   current.classList.add('show-article');
+
+  // change checked season input
+  const prevBtn = [...seasonBtns].find(el => el.checked);
+  prevBtn.checked = prevBtn.disabled = false;
+  seasonBtns[currentArticle].checked = true;
+  seasonBtns[currentArticle].disabled = true;
 };
 
+// main part
 seasonBtns.forEach((el, i) => {
   el.setAttribute('data-numseason', i);
   el.addEventListener('click', onClickSeasonBtn);
 });
 
-// seasonArticles.forEach((el, i) => {
-//   el.setAttribute('data-seasonart', i);
-//   idTimeout[i] = setTimeout(setIntervalCustom, SHOW_DELAY * i, i);
-// });
-
 seasonArticles.forEach((el, i) => {
   el.setAttribute('data-seasonart', i);
-  //   idInterval[i] = setInterval(setIntervalCustom, SHOW_DELAY * i, i);
 });
-const id = setInterval(showArticle, SHOW_DELAY);
+handleInterval();
