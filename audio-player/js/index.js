b6d605d94d49029a501ee8a8d0ee2264e2d84d65
Oleksii Bolotin <samkiral73@gmail.com>
2023-09-18 01:36:45 +0200

    feat: add styles for bg,add ticker (Mon, Sep 18, 2023 01:36:45 AM)

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,23 +1,35 @@
 import def from './ref.js';
 import { volumes } from './volumes.js';
 
-const { range, startBtn } = def;
+const { range, startBtn, descLink, container, view, bg } = def;
 
 let intervalId,
-  current = 0;
+  current = 0,
+  shift = 0;
 const audio = new Audio('./assets/songs/EdSheeran–Shape_of_You.mp3');
+
+const formDescription = ({ singer, name, genre, year }) => {
+  return new Array(30)
+    .fill(`${singer} "${name}" &nbsp; &nbsp; &nbsp; &nbsp;`)
+    .join('');
+};
 const checkRange = () => {
   if (audio.duration === audio.currentTime) {
     audio.pause();
     clearInterval(intervalId);
     intervalId = null;
     range.value = 0;
+    shift = 0;
     current = ++current % 3;
     audio.src = './assets/songs/' + volumes[current].path;
+    descLink.innerHTML = formDescription(volumes[current]);
+    bg.style.backgroundImage =
+      view.style.backgroundImage = `url(./assets/images/${volumes[current].cover})`;
     startStop();
     return;
   }
   range.value = (100 * audio.currentTime) / audio.duration;
+  descLink.style.transform = `translateX(-${++shift}vw)`;
 };
 
 const onRange = e => {
@@ -31,9 +43,15 @@ const startStop = () => {
   if (startBtn.checked) {
     if (!intervalId) intervalId = setInterval(checkRange, 1000);
     audio.play();
-  } else audio.pause();
+    descLink.classList.add('visible');
+  } else {
+    descLink.classList.remove('visible');
+    audio.pause();
+  }
 };
 
+descLink.innerHTML = formDescription(volumes[0]);
+bg.style.backgroundImage =
+  view.style.backgroundImage = `url(./assets/images/${volumes[0].cover})`;
 startBtn.addEventListener('input', startStop);
 range.addEventListener('input', onRange);
-// debugger;
--- audio-player/js/ref.js
+++ audio-player/js/ref.js
@@ -1,6 +1,9 @@
 const range = document.querySelector('#range');
 const startBtn = document.querySelector('#checkbox');
-const q = document.querySelector('#range');
+const descLink = document.querySelector('#description');
+const container = document.querySelector('.container');
+const view = document.querySelector('.view');
+const bg = document.querySelector('.bg');
 
-const ref = { range, startBtn };
+const ref = { range, startBtn, descLink, container, view, bg };
 export default ref;
