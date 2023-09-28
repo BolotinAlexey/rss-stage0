c32bae86d28c18fcfe3ef69de9354663856689d5
Oleksii Bolotin <samkiral73@gmail.com>
2023-09-28 19:21:06 +0200

    feat: added preliminary styles (Thu, Sep 28, 2023 07:21:06 PM)

---
+++ game-1024/js/getRefs.js
@@ -0,0 +1,5 @@
+export default function getRefs() {
+  return {
+    area: document.querySelector('#area'),
+  };
+}
--- game-1024/js/item.js
+++ game-1024/js/item.js
@@ -1,7 +1,16 @@
+import getRefs from './getRefs.js';
+const refs = getRefs();
 export default class Item {
   constructor(x, y, value) {
     this.x = x;
     this.y = y;
     this.value = value;
+
+    // create div
+    this.node = document.createElement('div');
+    this.node.classList.add('area__item');
+    this.node.style.top = `${110 * y}px`;
+    this.node.style.left = `${110 * x}px`;
+    refs.area.appendChild(this.node);
   }
 }
