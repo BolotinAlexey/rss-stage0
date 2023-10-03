b57ed87d221c78c2151fb70a10880aeb04298382
Oleksii Bolotin <samkiral73@gmail.com>
2023-10-03 17:23:40 +0200

    feat: add change offset score.value when >99,>999,.. (Tue, Oct 03, 2023 05:23:40 PM)

--- random-game/js/animationScore.js
+++ random-game/js/animationScore.js
@@ -2,6 +2,12 @@ import getRefs from './getRefs.js';
 
 const refs = getRefs();
 export default function animationScore(old, now) {
+  if (old.toString().length !== now.toString().length) {
+    if (now > 99) refs.score.style.right = '-90px';
+    if (now > 999) refs.score.style.right = '-120px';
+    if (now > 9999) refs.score.style.right = '-150px';
+    if (now > 99999) refs.score.style.right = '-180px';
+  }
   const t = 80;
 
   const intID = setInterval(() => {
